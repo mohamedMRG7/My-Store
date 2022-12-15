a69c1756d2987af1946dc8dbc2f67ee4519618a7
mohamed ragab <mohamad1325@gmail.com>
2022-12-15 20:24:39 +0200

    init app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { ToolBarComponent } from './shared/tool-bar/tool-bar.component';
+import { ProductItemComponent } from './product/product-item/product-item.component';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolBarComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, MatSlideToggleModule, MatToolbarModule, MatIconModule, MatBadgeModule, MatCardModule,MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
