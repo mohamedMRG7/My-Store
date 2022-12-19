28262ad90cffa83bc8731282b47f4a5864a4bec4
mohamed ragab <mohamad1325@gmail.com>
2022-12-19 13:58:29 +0200

    Fix issues

--- src/app/cart/cart-info/cart-info.component.ts
+++ src/app/cart/cart-info/cart-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CartService } from 'src/app/services/cart/cart.service';
 
@@ -7,19 +7,26 @@ import { CartService } from 'src/app/services/cart/cart.service';
   templateUrl: './cart-info.component.html',
   styleUrls: ['./cart-info.component.css']
 })
-export class CartInfoComponent implements OnInit {
+export class CartInfoComponent implements OnInit,DoCheck {
 
   stepperEditable = true;
   paymentFormGroup = new FormGroup({
     fullName: new FormControl('', [Validators.required, Validators.minLength(3)]),
     address: new FormControl('', [Validators.required, Validators.minLength(6)]),
     creditNumber: new FormControl('', [Validators.required, Validators.minLength(16), Validators.maxLength(16), Validators.pattern('^[0-9]*$')])
-  });
+    
 
+  });
+  items= new FormControl(this.itemsCount, [Validators.min(1)])
 
   isLinear = false;
 
   constructor(private _formBuilder: FormBuilder, private cartService: CartService) { }
+
+  ngDoCheck(): void {
+    
+    this.items.setValue(this.itemsCount())
+  }
 
   ngOnInit(): void {
   }
